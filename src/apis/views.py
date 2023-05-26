51b6035f5d48eb720fa89b46cc9fb6a7fa54e3b2
Albie Samson <albie0019@gmail.com>
2023-05-26 14:40:51 -0700

    Added two endpoints "http://127.0.0.1:8000/generate" "http://127.0.0.1:8000/verify"

--- src/apis/views.py
+++ src/apis/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+# SRS_CMP2 page 18
+@api_view(['POST'])
+def generate(request):
+    # Do stuff to generate schedules here
+    return Response({"message": "200 OK"})
+
+
+# SRS_CMP2 page 19
+@api_view(['POST'])
+def verify(request):
+    # Do stuff to verify schedules here
+    return Response({"message": "200 OK"})

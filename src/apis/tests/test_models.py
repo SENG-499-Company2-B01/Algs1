fbc312d68795b8ec393c76bc32692471e2883802
Dylan <dylanjkemp@hotmail.com>
2023-05-30 03:14:32 -0700

    update test

--- src/apis/tests/test_models.py
+++ src/apis/tests/test_models.py
@@ -24,9 +24,9 @@ def test_course_init():
 
 def test_change_courseInfo():
 	course = models.Course("SENG 275", 300, "FALL", "A01", "Prof", "9:00", "10:00", "MWF", "CAB 269", ["CSC 110"], ["SENG 250"], False)
-	course.changeInfo(new_course_size = 300, new_section = "A02", new_end_time = "11:00", new_classroom = "ECS 269", new_prereqs = ["CSC 110", "CSC 120"], new_core = True)
+	course.changeInfo(new_course_size = 350, new_section = "A02", new_end_time = "11:00", new_classroom = "ECS 269", new_prereqs = ["CSC 110", "CSC 120"], new_core = True)
 	assert course.course_name == "SENG 275"
-	assert course.course_size == 300
+	assert course.course_size == 350
 	assert course.session == "FALL"
 	assert course.section == "A02"
 	assert course.prof == "Prof"

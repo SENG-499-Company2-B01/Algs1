a13eebb81460d73325c6b5404a54fa568a3fda7d
Albie Samson <albie0019@gmail.com>
2023-05-29 17:50:45 -0700

    Created Tests folder for better organization in the future.

---
+++ src/apis/Tests/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from apis.models import Course
+
+# Create your tests here.
+
+# Test1: Test for ChangeInfo
+
+# Test2: Test for getSession
+
+# Test3: Test for addPrereq
+
+# Test4: Test for addCoreq
+
+# Test5: Test for getOutputDict
+
+# Test6: Test for cloneToSession
+
+# Test7: Test for addSection

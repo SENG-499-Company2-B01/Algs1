299df41807d130c4d294bee8117cf7680808728d
Dylan <dylanjkemp@hotmail.com>
2023-05-30 03:12:41 -0700

    add some model.py unit tests

--- src/apis/tests/test_models.py
+++ src/apis/tests/test_models.py
@@ -22,7 +22,7 @@ def test_course_init():
 	assert course.coreqs == ["SENG 250"]
 	assert course.core == True
 
-def test_change_course_info():
+def test_change_courseInfo():
 	course = models.Course("SENG 275", 300, "FALL", "A01", "Prof", "9:00", "10:00", "MWF", "CAB 269", ["CSC 110"], ["SENG 250"], False)
 	course.changeInfo(new_course_size = 300, new_section = "A02", new_end_time = "11:00", new_classroom = "ECS 269", new_prereqs = ["CSC 110", "CSC 120"], new_core = True)
 	assert course.course_name == "SENG 275"
@@ -39,8 +39,15 @@ def test_change_course_info():
 	assert course.core == True
 # Test2: Test for getSession
 
-# Test3: Test for addPrereq
+def test_getSession():
+	course = models.Course("SENG 275", 300, "FALL", "A01", "Prof", "9:00", "10:00", "MWF", "CAB 269", ["CSC 110"], ["SENG 250"], False)
+	assert course.getSession() == "FALL"
 
+# Test3: Test for addPrereq
+def test_addPrereq():
+	course = models.Course("SENG 275", 300, "FALL", "A01", "Prof", "9:00", "10:00", "MWF", "CAB 269", ["CSC 110"], ["SENG 250"], False)
+	course.addPrereq("CSC 120")
+	assert course.prereqs == ["CSC 110", "CSC 120"]
 # Test4: Test for addCoreq
 
 # Test5: Test for getOutputDict

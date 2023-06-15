529aa6b407a2c37b713cd521d6b90e4defb009fe
Jeffrey Li <jffr.li1@gmail.com>
2023-06-14 19:19:30 -0700

    Translated down to calculate_ideal_teacher_dispersion()

--- cso.py
+++ cso.py
@@ -64,7 +64,7 @@ class teacher_record:
 			availability_days: int = 0,
 			is_available_at_day: list = [1,1,1,1,1],
 			unavailable_timeslots: list = [1 for i in range(numSlots)],
-			classes_he_teachers: list = [[0 for i in range(3)] for j in range(classes_no1)],
+			classes_he_teaches: list = [[0 for i in range(3)] for j in range(classes_no1)],
 			num_of_classes: int = 0,
 			coteachings: list = [[0 for i in range(5)] for j in range(15)],
 			count_of_coteachers: int = 0):
@@ -76,7 +76,7 @@ class teacher_record:
 		self.availability_days = availability_days
 		self.is_available_at_day = is_available_at_day
 		self.unavailable_timeslots = unavailable_timeslots
-		self.classes_he_teachers = classes_he_teachers
+		self.classes_he_teaches = classes_he_teaches
 		self.num_of_classes = num_of_classes
 		self.coteachings = coteachings
 		self.count_of_coteachers = count_of_coteachers
@@ -178,4 +178,149 @@ def swap(a:list, class1: int, timeslow1: int, timeslot2: int):
 	a[class1][timeslot1] = a[class1][timeslot2]
 	a[class1][timeslot2] = temp
 	return 1
+
+def check_teachers_empty_periods(to_file: int, 
+		mode: int,
+		begin: int,
+		end: int,
+		a: list,
+		number_of_teachers1: int,
+		TEPW1: float,
+		show_results: int = 0
+		): # checks the teacher's empty periods and returns the relevant cost
+	total_cost = 0.0
+	start = 0
+	days = 0
+	cases_of_teachers = 0
+
+	total_gaps_of_teachers = 0
+
+	if mode == 0:
+		for i in range(number_of_teachers1):
+			cost = 0.0
+			total_gaps = 0
+			days = 0
+
+			for start in range(begin,end,7):
+				gaps = 0
+				teaching_hours = 0
+				last_lesson = start
+				first_lesson = start
+				flag = 1
+				has_lesson = 0
+
+				for t in range(start,start+7):
+					for j in range(teachers[i].num_of_classes):
+						if (a[teachers[i].classes_he_teaches[j][0]][t] == i):
+							has_lesson = 1
+							if flag == 1:
+								first_lesson = t
+								flag = 0
+							last_lesson = t
+							teaching_hours += 1
+							break
+
+				if has_lesson == 1:
+					gaps = last_lesson - first_lesson + 1 - teaching_hours
+					total_gaps += gaps
+
+				if gaps > 0:
+					total_gaps_of_teachers += gaps
+					days += 1
+
+			if days > 0:
+				cases_of_teachers += 1
+				cost = TEPW1 * total_gaps * pow(BASE,days)
+
+			if show_results == 1:
+				print(f"Teacher {teachers[i].surname} has {days} days with total gaps: {total_gaps}")
+			if to_file == 33:
+				fp3.write(f"Teacher {teachers[i].surname} has {days} days with total gaps: {total_gaps}")
+
+			total_cost += cost
+		if show_results == 1 or show_results == 2:
+			print(f"There are {cases_of_teachers} teachers with {total_gaps_of_teachers} total idle timeslots")
+		if to_file == 33:
+			fp3.write(f"There are {cases_of_teachers} teachers with {total_gaps_of_teachers} total idle timeslots")
+			fp3.write(f"Empty teachers timeslot cost is {total_cost}")
+		return total_cost
+
+	for i in range(number_of_teachers1):
+		cost = 0.0
+		total_gaps = 0
+		days = 0
+
+		for start in range(begin,end,start+7):
+			gaps = 0
+			teaching_hours = 0
+			last_lesson = start
+			first_lesson = start
+			flag = 1
+			has_lesson = 0
+
+			for t in range(start,start+7):
+				for j in range(teachers[i].num_of_classes):
+					class1 = teachers[i].classes_he_teaches[j][0]
+
+					if teachers[i].kind == 0:
+						if a[class1][t] == i:
+							has_lesson = 1
+							if flag == 1:
+								first_lesson = t
+								flag = 0
+							last_lesson = t
+							teaching_hours += 1
+					else:
+						co_class1 = co_class[class1][i]
+						if co_class1 == class1:
+							co_teacher1 = co_teacher[i][co_class1]
+						else:
+							co_teacher1 = 2015
+
+						if co_teacher1 < 0:
+							if a[class1][t] == i:
+								has_lesson = 1
+								if flag == 1:
+									first_lesson = t
+									flag = 0
+								last_lesson = t
+								teaching_hours += 1
+						else:
+							if a[class1][t] == i or a[class1][t] == co_teacher1:
+								has_lesson = 1
+								if flag == 1:
+									first_lesson = t
+									flag = 0
+								last_lesson = t
+								teaching_hours += 1
+								break
+
+			if has_lesson == 1:
+				gaps = last_lesson - first_lesson + 1 - teaching_hours
+				total_gaps += gaps
+			if gaps > 0:
+				total_gaps_of_teachers += gaps 
+				days += 1
+
+		if days > 0:
+			cases_of_teachers += 1
+			cost = TEPW1 * total_gaps * pow(BASE,days)
+
+		if show_results == 1:
+			print(f"Teacher {teachers[i].surname} has {days} days with total gaps: {total_gaps}")
+		if to_file == 33:
+			fp3.write(f"Teacher {teachers[i].surname} has {days} days with total gaps: {total_gaps}")
+
+		total_cost += cost
+
+
+	if show_results == 1 or show_results == 2:
+		print(f"There are {cases_of_teachers} teachers with {total_gaps_of_teachers} total idle timeslots")
+	if to_file == 33:
+		fp3.write(f"There are {cases_of_teachers} teachers with {total_gaps_of_teachers} total idle timeslots")
+		fp3.write(f"Empty teachers timeslot cost is {total_cost}")
+	return total_cost
+
+def calculate_ideal_teacher_dispersion(teacher11: int, total_hours1: int):
+	return 7 * total_hours1 / float(teachers[teacher11].availability_hours)
 

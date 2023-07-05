2d5d8e85a71920122cf2119927e8f869f9315c06
Johnathan <jhelfric@uvic.ca>
2023-07-05 10:22:11 -0700

    return proper schedule format

--- src/apis/views.py
+++ src/apis/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import json
 
 # TODO: Temporary import
 from .temp.dummy_schedule import get_dummy_schedule
@@ -13,9 +14,20 @@ from .temp.dummy_schedule import get_dummy_schedule
 def generate(request):
     # TODO: Do stuff to generate schedules here
     courses = get_dummy_schedule()
-    data = request.data
-
-    return Response(schedule)
+    jsonData = request.data
+    try:
+        term = jsonData["term"]
+        year = jsonData["year"]
+        schedule = {
+            "year": year, 
+            "terms": [{
+                "term": term,
+                "courses": courses
+            }]
+        }
+        return Response(schedule, status=200)
+    except Exception as e:
+        return Response(status=400)
 
 
 # SRS_CMP2 page 19

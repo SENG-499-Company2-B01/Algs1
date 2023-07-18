2f8a09783f599e922d6883c2222c3e824f19aa45
Johnathan <jhelfric@uvic.ca>
2023-07-18 12:05:40 -0700

    lower number of iterations to 10

--- src/apis/scheduler/cat_swarm.py
+++ src/apis/scheduler/cat_swarm.py
@@ -269,7 +269,7 @@ def cat_swarm_optimization(professors, classes, rooms, time_blocks, population_s
 def main(input_profs, input_courses, input_classrooms):
 
     population_size = 50
-    max_iterations = 100
+    max_iterations = 10
 
     with open('/app/time_blocks.json') as f:
         data = f.read()
